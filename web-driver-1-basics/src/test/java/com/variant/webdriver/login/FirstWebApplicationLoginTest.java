c1553109af49b90a70ce073e725be057973a0d18
Vibhor <guptavibhorit@gmail.com>
2020-08-27 01:40:17 +0530

    Added Webdriver Project - Basic

---
+++ web-driver-1-basics/src/test/java/com/variant/webdriver/login/FirstWebApplicationLoginTest.java
@@ -0,0 +1,41 @@
+package com.variant.webdriver.login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import com.variant.webdriver.basics.AbstractChromeWebDriverTest;
+
+public class FirstWebApplicationLoginTest extends AbstractChromeWebDriverTest{
+  
+  @Test
+  public void login() {
+	  driver.get("http://localhost:8080/login");
+	  
+	  sleep(5);
+	  
+	  WebElement nameElement = driver.findElement(By.name("name"));
+	  nameElement.sendKeys("in28minutes");
+	  
+	  sleep(2);
+	  
+	  WebElement passwordElement = driver.findElement(By.id("password"));
+	  passwordElement.sendKeys("dummy");
+	  
+	  sleep(2);
+	  
+	  WebElement submitElement = driver.findElement(By.id("submit"));
+	  submitElement.click();
+	  
+	  sleep(2);
+	  
+	  WebElement welcomeMessageElement = 
+			  driver.findElement(By.id("welcome-message"));
+	  
+	  
+	  
+	  //Welcome in28minutes!! Click here to manage your todo's.
+	  System.out.println(welcomeMessageElement.getText());
+	  
+  }
+}
